05eb3290426d0a31548fb63b4a1dc30c6b0f0eb5
dmarquina <mrdiego0892@gmail.com>
2024-05-10 21:02:44 -0500

    diario template

--- src/app/pages/journal/journal.component.ts
+++ src/app/pages/journal/journal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { EntryComponent } from "../../components/entry/entry.component";
 
 @Component({
-  selector: 'app-journal',
-  standalone: true,
-  imports: [],
-  templateUrl: './journal.component.html',
-  styleUrl: './journal.component.css'
+    selector: 'app-journal',
+    standalone: true,
+    templateUrl: './journal.component.html',
+    styleUrl: './journal.component.css',
+    imports: [EntryComponent]
 })
 export class JournalComponent {
 
--- src/app/pages/taskjournal/taskjournal.component.ts
+++ src/app/pages/taskjournal/taskjournal.component.ts
@@ -3,13 +3,14 @@ import { TaskListComponent } from "../tasklist/tasklist.component";
 import { JournalComponent } from "../journal/journal.component";
 import { CalendarComponent } from "../../components/shared/calendar/calendar.component";
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-taskjournal',
     standalone: true,
     templateUrl: './taskjournal.component.html',
     styleUrl: './taskjournal.component.css',
-    imports: [TaskListComponent, JournalComponent, CalendarComponent,MatTabsModule]
+    imports: [TaskListComponent, JournalComponent, CalendarComponent,MatTabsModule,MatIconModule]
 })
 export class TaskJournalComponent {
 
